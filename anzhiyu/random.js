9eecac299c0545a3ec947b266be24021fd3b86b1
Cookie987 <bili.cookie987@outlook.com>
2024-04-03 11:22:37 +0000

    Merge pull request #123 from Cookie987/dependabot/npm_and_yarn/axios-1.6.8
    Bump axios from 1.6.7 to 1.6.8

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/hexo-butterfly-blog.html","posts/how-to-install-win11.html","posts/test-waline.html","posts/winserver2022.html","posts/hello-world.html","posts/pandownjs-butterfly.html","posts/notice.html","posts/cloudflare-redirect.html","posts/linkrule.html","posts/obs-vcam-vmic.html","posts/2594b0ef.html"];function toRandomPost(){
+var posts=["posts/how-to-install-win11.html","posts/test-waline.html","posts/winserver2022.html","posts/hexo-butterfly-blog.html","posts/hello-world.html","posts/pandownjs-butterfly.html","posts/notice.html","posts/cloudflare-redirect.html","posts/linkrule.html","posts/obs-vcam-vmic.html","posts/2594b0ef.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
